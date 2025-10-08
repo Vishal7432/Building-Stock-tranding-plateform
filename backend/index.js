19141cddabeb13a7f2420fad7c51c7af9f461a13
Vishal Gupta <vishalgupta10852@gmail.com>
2025-10-09 01:19:13 +0530

    Worked on database setup

--- backend/index.js
+++ backend/index.js
@@ -5,11 +5,97 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const mongoose = require("mongoose");
 
+const { HoldingsModel } = require("./models/HoldingsModel");
+const { PositionModel } = require("./models/PositionsModel");
+
 const PORT = process.env.PORT || 3000;
 const uri = process.env.MONGO_URL;
 
 const app = express();
 mongoose.connect;
+
+// app.get("/addHoldings", async (req, res) => {
+//   let tempHoldings = [
+//     {
+//       name: "BHARTIARTL",
+//       qty: 2,
+//       avg: 538.05,
+//       price: 541.15,
+//       net: "+0.58%",
+//       day: "+2.99%",
+//     },
+//     {
+//       name: "HDFCBANK",
+//       qty: 2,
+//       avg: 1383.4,
+//       price: 1522.35,
+//       net: "+10.04%",
+//       day: "+0.11%",
+//     },
+//     {
+//       name: "HINDUNILVR",
+//       qty: 1,
+//       avg: 2335.85,
+//       price: 2417.4,
+//       net: "+3.49%",
+//       day: "+0.21%",
+//     },
+//   ];
+
+//   tempHoldings.forEach((item) => {
+//     let newHolding = new HoldingsModel({
+//       name: item.name,
+//       qty: item.qty,
+//       avg: item.avg,
+//       price: item.price,
+//       net: item.day,
+//       day: item.day,
+//     });
+
+//     newHolding.save();
+//   });
+//   res.send("done!");
+// });
+
+app.get("/addPositions", async (req, res) => {
+  let tempPositions = [
+    {
+      product: "CNC",
+      name: "EVEREADY",
+      qty: 2,
+      avg: 316.27,
+      price: 312.35,
+      net: "+0.58%",
+      day: "-1.24%",
+      isLoss: true,
+    },
+    {
+      product: "CNC",
+      name: "JUBLFOOD",
+      qty: 1,
+      avg: 3124.75,
+      price: 3082.65,
+      net: "+10.04%",
+      day: "-1.35%",
+      isLoss: true,
+    },
+  ];
+
+  tempPositions.forEach((item) => {
+    let newPosition = new PositionModel({
+      product: item.product,
+      name: item.name,
+      qty: item.qty,
+      avg: item.avg,
+      price: item.price,
+      net: item.net,
+      day: item.day,
+      isLoss: item.isLoss,
+    });
+    newPosition.save();
+  });
+  res.send("done!");
+});
 
 app.listen(PORT, () => {
   console.log("Server is running on port 3000");
